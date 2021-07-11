ca8eb917a272e9af0f09fef90120b766bcce0f5d
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-07-11 20:41:09 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'Alpeza'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_book_theme'
+    
 ]
 
 # Add any paths that contain templates here, relative to this directory.
